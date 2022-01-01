ea42b3487a61551c6346c77c9a6cbce448554c0f
André Ribeiro <andre.sn@live.com>
2022-01-01 06:02:51 -0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: const Text("Transferências"),
           ),
+          body: const ListaTransferencias(),
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
             child: const Icon(Icons.add),
@@ -13,3 +15,43 @@ void main() => runApp(
         ),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia{
+  Transferencia(this.valor, this.numeroConta);
+
+  final double valor;
+  final int numeroConta;
+}
